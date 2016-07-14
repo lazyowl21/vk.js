cbed3ee7fbc659828f06dccbf2e97bad76c4a257
lazyowl21 <kashirin.e@list.ru>
2016-07-14 13:09:12 +0300

    http requests via "requests" module default parameters

--- vkapi.js
+++ vkapi.js
@@ -9,64 +9,56 @@ var express = require('express');
 
 var EventEmitter = require('events');
 
+var request = require('request').defaults({timeout:20000});
+var httpErr = require('http-errors');
+
 var apiv='5.52'
 var  codeExpiresIn = 3600*1000;
 
-function requestBody( opts ){
-logger.debug('requestBody: request: ', opts);
-
-return new Promise( ( resolve, reject ) => {
-	var req = http.request( opts, res => {
-		if( res.statusCode != 200 )
-			return reject( { code:'HTTP_ERROR', message:res.statusMessage } );
-
-		var databuf = "";
-		
-		res.on( 'data', chunk => { databuf+=chunk;  } );
-		res.on( 'end' , () => {
-			logger.debug('requestBody: response: ', databuf.toString('utf8'));
-			return resolve( databuf ); 
-		});
-	});
-	req.end();
+var _oauthRequest = request.defaults({
+	baseUrl:	"https://oauth.vk.com",
+	method:		"POST",
+	body:		{},
+	json:		true
 });
+
+var _apiRequest = request.defaults({
+	baseUrl:	"https://api.vk.com/method",
+	method:		"POST",
+	body:		{},
+	json:		true,
+
+	followRedirects: true
+});
+
+
+function errFromResp( resp ){
+	return httpErr( resp.statusCode, resp.statusMessage ); 
 }
-
-
 
 
 function Request(method, opts = {}){
-	var reqOpts = {
-		hostname:'api.vk.com',
-		method:'POST',
-		path:'/method/' + method + '?' + query.stringify( opts ) 
-	}
-	
-	return requestBody( reqOpts )
-		.then( res => JSON.parse( res.toString('utf8') ) )
-		.then( function requestErrHandler( data ) {
-			if('response' in data) return data.response;
-			else {
-				var err = new Error( data.error.error_msg );
-				err.name = "VK_ERROR";
-				err.code = data.error.error_code;
-				err.request_params = data.error.request_params;
-				throw  err;
-			}
-		});
-}
+	return new Promise( (resolve,reject) => {
+		_apiRequest( { uri: method, qs: opts },  ( err, resp, body )=>{
+			if(resp.statusCode != 200){
+				throw errFromResp(resp);
+			}	
 
-function checkToken( token ){
-	return Request( "users.get" , {access_token:token} )
-		.then( data => {
-			if( data.length === 0 ){
-				var err = new Error('Token seems to be invalid (empty users.get result)');
-				err.name = "VK_ERROR";
+			if( 'response' in body ){
+				resolve(body.response);
+			}
+			else if( 'error' in body ){
+				let err = new Error( body.error.error_msg );
+				err.code = "VK_ERROR";
+				err.name = "VKRequestError";
 				throw err;
 			}
-			else return data[0].id;
-		});	
+		}).on('error', reject);
+	});
+
+	
 }
+
 
 class App extends EventEmitter{	
 	constructor( opts ){
@@ -75,22 +67,29 @@ class App extends EventEmitter{
 	if( typeof opts !== 'object' ) throw new Error( "Invalid options type: " + typeof opts );
 	if(! ('appid' in opts) ) throw new Error( "No app id in options" );
 
+	// default parameters
+	this.hideTokens = true;
+	this.scope = [];
+
+	this.useSecureRequests = true;
+
 	Object.assign( this, opts );
 
 	this._getCode = this._getCode.bind(this);
 	this.setCode = this.setCode.bind(this);
+	this.setToken = this.setToken.bind(this);
 
 	this.token = false;
 	this.code = false;
 
-	this._codeExpiresIn = 3600*1000;
+	this.on('codeAccepted', this._requestToken );	
 
 	this._serviceToken = false;
 
 	this.Router = express.Router();
 	this.Router.get( '/vkauth' , this._getCode );
 	
-	this._requestServiceToken( err => {
+	if( this.useSecureRequests ) this._requestServiceToken( err => {
 		if( err )	logger.debug( err );
 	});
 	}
@@ -103,12 +102,11 @@ class App extends EventEmitter{
 		else {
 			var redirect_back = this.host + req.path
 			this.redirect_uri = redirect_back;
-
 			var redirect = "https://oauth.vk.com/authorize?" + query.stringify({
 				client_id:this.appid,
 				display:'page',
 				redirect_uri:redirect_back,
-				scope:['groups'],
+				scope: this.scope,
 				response_type:'code'
 			});
 			res.redirect(redirect); 
@@ -116,25 +114,23 @@ class App extends EventEmitter{
 	}
 	
 	_requestServiceToken( cb ){
-		logger.debug('Requesting service token');
-		requestBody("https://oauth.vk.com/access_token?" + query.stringify({
-			client_id:		this.appid,
-			client_secret:		this.appkey,
-			grant_type:		"client_credentials",
-			v:			this.v
-		})).then(
-			response => {
-				var data = {};
-				try{
-					data = JSON.parse(response.toString('utf8'));
-				}
-				catch(e){
-				}
-				if( 'access_token' in data ){
-					logger.debug('Service Token found in response');
-					this._serviceToken = data.access_token;
+		_oauthRequest({
+			uri:	"access_token",
+			qs:	{
+				client_id:		this.appid,
+				client_secret:		this.appkey,
+				grant_type:		"client_credentials",
+				v: this.v
+				},
+			json:	true
+			}, ( err, resp, body )=> {
+				if( 'access_token' in body ){
+					var token = body.access_token;
+
+					this.serviceToken = (this.hideTokens)? true : token;
+
 					this.secureRequest = function( method, opts = {} ){
-						opts.access_token = this._serviceToken;
+						opts.access_token = token;
 						opts.client_secret = this.appkey;
 						return Request( method, opts );
 					}.bind(this);
@@ -142,66 +138,59 @@ class App extends EventEmitter{
 					this.emit('serviceTokenAccepted');
 					return cb( null );
 				}
-				else if( 'error' in data ){
+				else if( 'error' in body ){
 					logger.debug( data );		
 				}
-			},
+		});				
 
-			error => {
-				logger.debug("Error caught");
-				return cb( error );
-			}
- 
-
-		);
-	
-	}
+		logger.debug('Requesting service token');
+		}
 
 	_requestToken(){
 		logger.debug("Requesting token");
-		var reqOpts = {
-			host:'oauth.vk.com',
-			method:'GET',
-			path:'/access_token?' + query.stringify({
+		
+		_oauthRequest({
+			uri:	"access_token",
+			qs:	{
 				client_id:this.appid,
 				client_secret:this.appkey,
 				redirect_uri:this.redirect_uri,
-				code:this.code
-			})
-		};
-
-		requestBody( reqOpts )
-			.then( resp => JSON.parse( resp.toString('utf8') ))
-			.then( data => {
-				if( 'access_token' in data ){
-					this.token = data.access_token;
-
-					this.tokenExpired = false;
-					this._tokenExpiresIn = 1000*data.expires_in;
-					this._tokenExpireTime = new Date( Date.now() + this._tokenExpiresIn);
-					
-					if(  this._tokenExpiresIn > 0  ) setTimeout( ()=>{
-						this.token = false;
-						this.tokenExpired = true;
-						this.emit('tokenExpired');
-					},this._tokenExpiresIn );
-
-					this.Request = function(method, opts={}){
-						opts.access_token = this.token;
-						return Request(method, opts );
-					}.bind(this);
-
-					this.emit('tokenAccepted');
+				code:this.code		
 				}
-				else if('error' in data){
-					var err = new Error( data.error.error_msg );
-					err.name = "VK_ERROR";
-					err.code = data.error.error_code;
-					throw err;
-				}
-			})
-			.catch( err => console.log(err) );
-	}	
+		}, ( err, resp, data ) =>{
+			if( 'access_token' in data ){
+				this.setToken( data.access_token, data.expires_in );	
+			}
+			else if('error' in data){
+				var err = new Error( data.error.error_msg );
+				err.name = "VK_ERROR";
+				err.code = data.error.error_code;
+				throw err;
+			}	
+		});
+	}
+
+}
+
+App.prototype.setToken = function( token, exprsIn = 0 ){
+	this.token = (this.hideTokens)? true: token;
+	
+	this.tokenExpired = false;
+	this.tokenExpiresIn = 1000*exprsIn;
+	this.tokenExpireTime = new Date( Date.now() + this.tokenExpiresIn );
+	
+	if( exprsIn > 0 ) setTimeout( () =>{
+		this.token = false;
+		this.tokenExpired = true;
+		this.emit('tokenExpired');
+	}, this.tokenExpiresIn );
+
+	this.Request = function(method, opts={}){
+		opts.access_token = token;
+		return Request(method, opts );
+	}.bind(this);
+
+	this.emit('tokenAccepted');
 }
 
 App.prototype.setCode = function( code, exprsIn = codeExpiresIn ){
@@ -213,8 +202,6 @@ App.prototype.setCode = function( code, exprsIn = codeExpiresIn ){
 	}, exprsIn ); 
 			
 	this.emit('codeAccepted');
-	
-	this._requestToken();
 	return;
 }
 
